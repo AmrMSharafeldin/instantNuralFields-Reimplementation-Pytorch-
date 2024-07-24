43620a078736be7ded6c69ed32aa3dcb44336bd1
Amr M Sharafeldin <69557495+AmrMohamedSharaf@users.noreply.github.com>
2024-07-23 21:00:03 -0700

    Update __init__.py

--- nerf/__init__.py
+++ nerf/__init__.py
@@ -1,7 +1,8 @@
 import nerf.eval
 import nerf.utils
 import nerf.hash
-import nerf.mlp
+import nerf.mlp_net
 import nerf.model_trainer
 import nerf.plenoxels
 import nerf.volume_renderer
+import nerf.create_model
